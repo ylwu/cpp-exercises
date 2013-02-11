858738c35b4a8ed9308f28d5d958851f0b9fecc2
Yonglin Wu <yonglinwu92@gmail.com>
2013-02-11 07:59:07 -0500

    Initial Commit

---
+++ Exercises/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Exercises
+//
+//  Created by Jason Wu on 2/11/13.
+//  Copyright (c) 2013 Jason Wu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
